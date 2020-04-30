b84e5db653c8fc9f15c80907e5ff24738054e330
Anirudh Goel <ani050599@gmail.com>
2020-04-30 14:20:41 +0530

    Update Strings.cpp
    Added toUpper functionality
    void toUpper(char s[])

--- SourceCode/String/Strings.cpp
+++ SourceCode/String/Strings.cpp
@@ -4,6 +4,11 @@ void greetings(char hello[]){
   printf("\n%s ",hello);
 }
 
+void toUpper(char str[]){
+  printf("Upper Case of string is %s",str);
+}
+
+
 int main(){
  char greet[] = "Hello World!";
  greetings(greet);
